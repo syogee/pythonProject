43261c4a621e2ad8ca7d4e2c7f5c667b6f52f357
yogi <yogi1891999@gmail.com>
2023-07-22 16:18:19 +0530

    repeating time

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 num = int(input("enter a no"))
-factorial = 1
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
+a = int(input("how many times"))
+b=0
+while b<a:
+  factorial = 1
+  if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+  elif num == 0:
+    print("The factorial of 0 is 1")
+  else:
+    for i in range(1,num + 1):
        factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+  print("The factorial of",num,"is",factorial)
+  b+=1
